0995ac644e3c0ef44ff231fd549911544035388a
tomoya.mikami <trymorecc@gmail.com>
2020-08-29 15:58:17 +0900

    expo install sensor

--- src/index.tsx
+++ src/index.tsx
@@ -12,9 +12,9 @@ const App = (): React.ReactElement => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 
